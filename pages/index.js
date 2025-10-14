7a01fc06e7599b27a01d24267b91c5a05f900a6a
itsBhx <francisco.a.ramos27@gmail.com>
2025-10-14 02:14:28 +0100

    New Rollback

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-mport { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 /* ======================= Time (Lisbon / WEST) helpers ======================= */
 const LISBON_TZ = "Europe/Lisbon";
