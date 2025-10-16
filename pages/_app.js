43457197a25e9aa9db436059a829534269ddf9fd
itsBhx <francisco.a.ramos27@gmail.com>
2025-10-16 19:09:40 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import Head from "next/head";
-import { Toaster } from "react-hot-toast";
+import { Toaster, toast } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
   return (
