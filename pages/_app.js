3f454c481969d4285d7293bb4b587d2e8f370d45
itsBhx <francisco.a.ramos27@gmail.com>
2025-10-16 18:47:02 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,7 +13,28 @@ export default function App({ Component, pageProps }) {
         />
         <link rel="icon" href="/logo.png" type="image/png" />
       </Head>
+
       <Component {...pageProps} />
+
+      {/* Global Toast System */}
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 2500,
+          style: {
+            background: "#1a1a1d", // matches your dark theme
+            color: "#fff",
+            border: "1px solid #3a3a3f",
+            fontWeight: 500,
+          },
+          success: {
+            iconTheme: {
+              primary: "#f79c42", // your orange glow
+              secondary: "#1a1a1d",
+            },
+          },
+        }}
+      />
     </>
   );
 }
